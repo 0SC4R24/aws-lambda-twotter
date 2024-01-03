3e8227f56e36b90b5f96ed117d975912957ca8b9
adriantoral <adrian.rubiboy11@gmail.com>
2024-01-03 22:42:19 +0100

    Agrega la funcion para ver los likes de un usuario Actualiza el frontend

---
+++ source/backend/view_likes/lambda_function.py
@@ -0,0 +1,81 @@
+import json
+from datetime import datetime
+
+import jwt
+import pymysql
+
+SECRET = "PROYECTO3SISDIS2024"
+# Encode token: jwt.encode({"some": "payload"}, "secret", algorithm="HS256")
+# Decode token: jwt.decode(encoded, "secret", algorithms=["HS256"])
+
+rds_host = "52.204.152.143"
+username = "practica3"
+password = "practica3"
+database = "practica3"
+bucketUrl = "https://0sc4r24sisdis2024.s3.us-east-1.amazonaws.com/"
+
+
+def lambda_handler(event, context):
+    # if "isBase64Encoded" in event:
+    #     isEncoded = bool(event["isBase64Encoded"])
+    #
+    #     if isEncoded:
+    #         decodedBytes = base64.b64decode(event["body"])
+    #         decodedStr = decodedBytes.decode("ascii")
+    #         print(json.dumps(parse_qs(decodedStr)))
+    #         decodedEvent = json.loads(json.dumps(parse_qs(decodedStr)))
+    #         user = decodedEvent["user"][0]
+    #
+    # else:
+    #     user = event["body"]["user"]
+
+    print(event)
+
+    body = json.loads(event.get("body", "{}"))
+
+    token = body.get('token', None)
+
+    if not token:
+        return {
+            'statusCode': 400,
+            'headers': {'Access-Control-Allow-Origin': '*'},
+            'body': 'No token provided'
+        }
+
+    try:
+        decoded = jwt.decode(token, SECRET, algorithms=["HS256"])
+        user_id = decoded.get('id')
+        exp = decoded.get('exp')
+
+        if datetime.now().timestamp() > exp:
+            return {
+                'statusCode': 401,
+                'headers': {'Access-Control-Allow-Origin': '*'},
+                'body': "Token has expired"
+            }
+
+    except:
+        return {
+            'statusCode': 401,
+            'headers': {'Access-Control-Allow-Origin': '*'},
+            'body': "Invalid token"
+        }
+
+    conn = pymysql.connect(rds_host, user=username, passwd=password, db=database, connect_timeout=5)
+    cursor = conn.cursor()
+
+    likes = [
+        {
+            'message_id': x[0],
+            'type': x[1]
+        } for x in cursor.fetchall()
+    ] if cursor.execute(f'SELECT message_id, type FROM likes WHERE user_id = %s', (user_id,)) else []
+
+    cursor.close()
+    conn.close()
+
+    return {
+        'statusCode': 200,
+        'headers': {'Access-Control-Allow-Origin': '*'},
+        'body': json.dumps(likes)
+    }
